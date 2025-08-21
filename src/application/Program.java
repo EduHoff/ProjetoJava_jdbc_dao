d943f91a8a97b8c0d223d5f7bfb7722236070528
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2025-08-21 13:35:58 -0300

    add metodo FindById e algumas pequenas correções no código

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 
+import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -11,14 +12,14 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "book");
-		
-		Seller seller = new Seller(21, "Ed", "ed@gmail.com", new Date(), 3000.0, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 		
+		DB.closeConnection();
 
 	}
 
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -11,7 +11,7 @@ public class Seller implements Serializable {
 	private Integer id;
 	private String name;
 	private String email;
-	private Date date;
+	private Date birthDate;
 	private Double baseSalary;
 	
 	private Department department;
@@ -22,11 +22,11 @@ public class Seller implements Serializable {
 		
 	}
 
-	public Seller(Integer id, String name, String email, Date date, Double baseSalary, Department department) {
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
-		this.date = date;
+		this.birthDate = birthDate;
 		this.baseSalary = baseSalary;
 		this.department = department;
 	}
@@ -55,12 +55,12 @@ public class Seller implements Serializable {
 		this.email = email;
 	}
 
-	public Date getDate() {
-		return date;
+	public Date getBirthDate() {
+		return birthDate;
 	}
 
-	public void setDate(Date date) {
-		this.date = date;
+	public void setBirthDate(Date date) {
+		this.birthDate = date;
 	}
 
 	public Double getBaseSalary() {
@@ -81,7 +81,7 @@ public class Seller implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", date=" + date + ", baseSalary="
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
 				+ baseSalary + ", department=" + department + "]";
 	}
 
